632d053a97ea808b84c996f14431f5d2456faf6a
gulam160 <gulam.mu159@gmail.com>
2023-01-17 22:44:28 +0530

    Project setup done

---
+++ optlap-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="App-nav"></nav>
+      <main className="App-main"></main>
+      <footer className="App-footer"></footer>
+    </div>
+  );
+}
+
+export default App;
